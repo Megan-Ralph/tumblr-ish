73b321e6ef922834cf2c05206871d0a0a174bdcb
Megan-Ralph <megan.ralph@simplybusines.co.uk>
2023-04-19 21:35:56 +0100

    Make articles and events editable

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def show
     @user = current_user
+    @posts = (Article.all.where(user: current_user) + Event.all.where(user: current_user)).sort_by(&:created_at)
   end
 end

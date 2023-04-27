8854ae5e5e80205ef9a4b34a66d083292c83614c
Megan-Ralph <megan.ralph@simplybusines.co.uk>
2023-04-27 14:42:17 +0100

    Prevent page from reloading

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,5 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import * as bootstrap from "bootstrap"
+import $ from 'jquery'
 import "@hotwired/turbo-rails"
 import "./controllers"
